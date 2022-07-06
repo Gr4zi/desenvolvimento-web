b3a6f926d6d81070b1822a63142b026984d4bfb3
Grazielle da Silva Ribeiro <71351284+Gr4zi@users.noreply.github.com>
2022-07-06 20:32:55 -0300

    Add files via upload

---
+++ Formulários - Validação de Formulários e Partes de um documento HTML/recebe-validacao.php
@@ -0,0 +1,34 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sem título</title>
+</head>
+
+<body>
+
+<?php 
+
+
+echo $_POST['estado']; 
+echo "</br>";	
+		
+// Verifica se usuário escolheu vários estados
+if(isset($_POST["estados"]))
+{
+    echo "O(s) Estadoso(s) selecionados são:<br>";
+    // Faz loop para os livros
+    foreach($_POST["estados"] as $estados)
+    {
+        echo "- " . $estados . "<br>";
+    }
+}
+else
+{
+    echo "Você não escolheu nenhum estado!";
+}
+	
+?>
+
+</body>
+</html>
